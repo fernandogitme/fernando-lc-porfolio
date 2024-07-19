fb10067f74b58c79069124abcf0a504a3918598b
fernando <fernandolucasclaro@hotmail.com>
2024-07-19 18:55:35 +0200

    mejor responsive

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -29,9 +29,9 @@ const ServicesPage = () => {
             {isWideScreen && <CircleImage />}
             {isWideScreen && <AvatarServices />}
             <ContainerPageServices>
-                <div className="grid items-center justify-center  gap-2 mx-auto max-w-5xl md:grid-cols-2">
+                <div className="grid items-center justify-center space-y-36 gap-3 mx-auto max-w-5xl md:grid-cols-2">
                     <div className="max-w-[450px]">
-                        <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
+                        <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5 ">
                             Mis <span className="font-bold text-secondary"> Competencias.</span>
                         </h1>
                         <p className="mb-3 text-xl text-gray-300">
--- components/container-page-services.tsx
+++ components/container-page-services.tsx
@@ -6,7 +6,7 @@ const ContainerPageServices = (props: ContainerPageProps) => {
     const { children } = props;
 
     return (
-        <div className="w-full h-screen flex items-center justify-center px-4 pb-10 mx-auto mt-40 md:pb-0 md:px-6">
+        <div className="w-full flex items-center justify-center px-4 pb-10 mx-auto mt-40 md:pb-0 md:px-6">
             {children}
         </div>
     );
